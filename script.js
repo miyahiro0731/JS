725063ff4ceb49f8c8d1bd9c741f43bbd2debfb8
miyahiro0731 <118321330+miyahiro0731@users.noreply.github.com>
2023-01-15 22:40:48 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let lists = document.getElementsByClassName("pull-down-list");
-console.log(lists);
+window.addEventListener('load',function(){
+    const pullDownButton = document.getElementById("lists")
+    console.log("読み込みました")
+})
+
+document.addEventListener('click',function(){
+    const mouseOverButton = document.getElementById('lists')
+    console.log("クリックされた")
+})
+
+
+document.addEventListener('mouseover',function(){
+    const mouseOverButton = document.getElementById('lists')
+    console.log("乗った")
+})
+
+document.addEventListener('mouseout',function(){
+    const mouseOutButton = document.getElementById("list")
+    console.log("離れた")
+})
