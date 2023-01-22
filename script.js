d790cdf87121307f5fd3cc0558d556872ba22b3b
miyahiro0731 <118321330+miyahiro0731@users.noreply.github.com>
2023-01-22 21:51:36 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ window.addEventListener('load',function(){
     console.log("読み込みました")
 
     pullDownButton.addEventListener('click',function(){
-        console.log("クリックされた")
+        console.log("クリックしたときは緑色")
+        pullDownButton.setAttribute("style","background-color:green;")
     })
 
     pullDownButton.addEventListener('mouseover',function(){
-        console.log("乗った")
+        console.log("乗ったときは青色")
         pullDownButton.setAttribute("style","background-color:blue;")
     })
 
     pullDownButton.addEventListener('mouseout',function(){
-        console.log("離れた")
+        console.log("はずれたときは赤色")
+        pullDownButton.removeAttribute("style","background-color:red;")
     })
     
 })
