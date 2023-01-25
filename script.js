561ef08f8061b4621ca7df839bc3bf2775a1d5be
miyahiro0731 <118321330+miyahiro0731@users.noreply.github.com>
2023-01-25 20:40:02 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 window.addEventListener('load',function(){
     const pullDownButton = document.getElementById("lists")
+    const pullDownParents = document.getElementById("pull-down")
     console.log("読み込みました")
 
     pullDownButton.addEventListener('click',function(){
-        console.log("クリックしたときは緑色")
-        pullDownButton.setAttribute("style","background-color:green;")
+        if(pullDownParents.getAttribute("style")=="display:block;"){
+            pullDownParents.removeAttribute("style","display:block;")
+            console.log("非表示")
+        }else{
+            pullDownParents.setAttribute("style","display:block;")
+            console.log("表示")
+        }
     })
 
     pullDownButton.addEventListener('mouseover',function(){
+        this.setAttribute("style","background-color:blue;")
         console.log("乗ったときは青色")
-        pullDownButton.setAttribute("style","background-color:blue;")
     })
 
     pullDownButton.addEventListener('mouseout',function(){
-        console.log("はずれたときは赤色")
-        pullDownButton.removeAttribute("style","background-color:red;")
+        this.removeAttribute("style","background-color:red;")
+        console.log("外れたときは赤色")
     })
     
 })
