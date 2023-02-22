06418cfa6973ecbadc7fbe4d3d6a4fb6f5932c7a
miyahiro0731 <118321330+miyahiro0731@users.noreply.github.com>
2023-02-23 08:13:26 +0900

    PHP5

--- sample.php
+++ sample.php
@@ -1,12 +1,69 @@
 <?php
-    for($i=1;$i<15;$i++){
-        if($i % 14 === 0){
-            echo 'for_out';
-            break;
+    class Company{
+        protected $name;
+        protected $adress;
+        protected $number;
+        public function setName($name){
+            $this->name = $name;
         }
-        if($i % 2 === 0){
-            continue;
+        public function setAdress($adress){
+            $this->adress = $adress;
         }
-        echo $i ."\n";
+        public function setNumber($number){
+            $this->number = $number;
+        }
+        public function displayCompany(){
+            echo "会社名:".$this->name;
+            echo "\n";
+            echo "住所:".$this->adress;
+            echo "\n";
+            echo "電話番号:".$this->number;
+            echo "\n";
+        }
     }
+    class Department extends Company{
+        private $department;
+        private $leader;
+        public function setDepartment($department){
+            $this->department = $department;
+        }
+        public function setLeader($leader){
+            $this->leader = $leader;
+        }
+        public function displayDepartment(){
+            echo '部署名:'.$this->department;
+            echo "\n";
+            echo '部長:'.$this->leader;
+            echo "\n";
+        }
+    } 
+
+    $sampleA = new Department();
+    $sampleA->setName("サンプルA");
+    $sampleA->setAdress("市ヶ谷");
+    $sampleA->setNumber("080-XXXX-XXXX");
+    $sampleA->setDepartment("人事");
+    $sampleA->setLeader("高田");
+    $sampleB = new Department();
+    $sampleB->setName("サンプルB");
+    $sampleB->setAdress("池袋");
+    $sampleB->setNumber("090-XXXX-XXXX");
+    $sampleB->setDepartment("総務");
+    $sampleB->setLeader("田中");
+    $sampleC = new Department();
+    $sampleC->setName("サンプルC");
+    $sampleC->setAdress("新宿");
+    $sampleC->setNumber("070-XXXX-XXXX");
+    $sampleC->setDepartment("営業");
+    $sampleC->setLeader("高橋");
+
+    $companys = array($sampleA,$sampleB,$sampleC);
+
+    foreach($companys as $value){
+        $value->displayCompany();
+        $value->displayDepartment();
+        echo "\n";
+    }
+
+
  ?>
